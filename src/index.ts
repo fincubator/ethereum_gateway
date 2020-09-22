c2e3b144c73c6df48679008211a8d8da04a759ea
Sergey Sherkunov <leinlawun@leinlawun.org>
2020-09-22 17:32:31 +0300

    Fix create order RPC method
    Signed-off-by: Sergey Sherkunov <leinlawun@leinlawun.org>

--- src/index.ts
+++ src/index.ts
@@ -10,12 +10,12 @@ import { createTerminus } from '@godaddy/terminus';
 import { Server as WebSocketServer } from 'rpc-websockets';
 
 import { app, appConfig, onSignal, onStart } from './app';
-import { getDepositAddress, initNewTx, validateAddressRequest } from './rpc';
+import { getDepositAddress, newInTx, validateAddressRequest } from './rpc';
 
 const server = http.createServer(app);
 const rpc = new WebSocketServer({ server, path: '/ws-rpc' });
 
-rpc.register('init_new_tx', initNewTx, '/ws-rpc');
+rpc.register('init_new_tx', newInTx, '/ws-rpc');
 rpc.register('get_deposit_address', getDepositAddress, '/ws-rpc');
 rpc.register('validate_address_request', validateAddressRequest, '/ws-rpc');
 
